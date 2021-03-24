447a3724668c7cef5e1b5c6d350443a088402113
Carla Holcomb <carla.programmer@gmail.com>
2021-03-24 03:04:05 +0000

    Automatically backed up by Learn

--- db/migrate/47298573097492387_create_appointments.rb
+++ db/migrate/47298573097492387_create_appointments.rb
@@ -1,4 +1,4 @@
-class CreateAppointments < ActiveRecord::Migration[6.1]
+class CreateAppointments < ActiveRecord::Migration[5.0]
   def change
     create_table :appointments do |t|
       t.datetime :appointment_datetime
