f2732ad03a07736543a99efaedf3b659731adbac
Carla Holcomb <carla.programmer@gmail.com>
2021-03-24 03:03:31 +0000

    Automatically backed up by Learn

--- db/migrate/428342897492303798_create_patients.rb
+++ db/migrate/428342897492303798_create_patients.rb
@@ -1,9 +1,9 @@
 class CreatePatients < ActiveRecord::Migration[6.1]
   def change
     create_table :patients do |t|
-      t.datetime :appointment_datetime
-      t.integer :patient_id
-      t.integer :doctor_id
+      t.string :name
+      t.integer :age
+      
 
       t.timestamps
     end
